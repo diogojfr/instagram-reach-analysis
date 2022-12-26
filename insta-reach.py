e3733e591a089f4a56871006910f504d326450be
Diogo Reis <diogojfr1@gmail.com>
2022-12-26 11:46:52 -0300

    adding the notebook

---
+++ insta-reach.py
@@ -0,0 +1,8 @@
+import pandas as pd 
+import numpy as np 
+import matplotlib.pyplot as plt 
+import plotly.express as px
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import PassiveAgressiveRegressor
+
